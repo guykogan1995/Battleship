e9b677fb9fc6e927ea6dd6552aa05ae77a7722ce
guykogan1995 <89678482+guykogan1995@users.noreply.github.com>
2022-01-23 11:31:11 -0700

    Initial commit

---
+++ Battleship/task/src/battleship/Ships.java
@@ -0,0 +1,22 @@
+package battleship;
+
+public class Ships extends Actions {
+    private int length;
+    private String name;
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
